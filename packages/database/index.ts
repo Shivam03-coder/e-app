624c428567284cc0df6c5e7e59286a8d1c2471d6
shivam <shivam850anand@gmail.com>
2025-07-12 18:55:03 +0530

    fix : upstash error

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -10,7 +10,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-// Create or reuse instance
 export const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') {
