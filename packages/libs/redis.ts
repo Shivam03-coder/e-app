268f6a42720f6da005e9abec5944ec1620b732e3
shivam <shivam850anand@gmail.com>
2025-07-12 19:08:05 +0530

    redis config chnaged

--- packages/libs/redis.ts
+++ packages/libs/redis.ts
@@ -1,21 +1,35 @@
 import Redis from 'ioredis';
 
 class RedisService {
-  private client: Redis;
-  constructor() {
-    this.client = new Redis(process.env.REDIS_URL as string);
+  private static instance: RedisService;
+  private client: Redis | null = null;
 
-    this.client.on('connect', () => {
-      console.log('Redis connected');
-    });
+  private constructor() {}
 
-    this.client.on('error', (err) => {
-      console.error('Redis error:', err);
-    });
+  public static getInstance(): RedisService {
+    if (!RedisService.instance) {
+      RedisService.instance = new RedisService();
+    }
+    return RedisService.instance;
+  }
+
+  private initializeClient(): void {
+    if (!this.client) {
+      this.client = new Redis(process.env.REDIS_URL as string);
+
+      this.client.on('connect', () => {
+        console.log('Redis connected');
+      });
+
+      this.client.on('error', (err) => {
+        console.error('Redis error:', err);
+      });
+    }
   }
 
   public getClient(): Redis {
-    return this.client;
+    this.initializeClient();
+    return this.client!;
   }
 
   public async set(
@@ -23,20 +37,30 @@ class RedisService {
     value: string | number,
     expiryInSec?: number
   ): Promise<'OK'> {
+    this.initializeClient();
     if (expiryInSec) {
-      return this.client.set(key, value, 'EX', expiryInSec);
+      return this.client!.set(key, value, 'EX', expiryInSec);
     }
-    return this.client.set(key, value);
+    return this.client!.set(key, value);
   }
 
   public async get(key: string): Promise<string | null> {
-    return this.client.get(key);
+    this.initializeClient();
+    return this.client!.get(key);
   }
 
   public async del(key: string): Promise<number> {
-    return this.client.del(key);
+    this.initializeClient();
+    return this.client!.del(key);
+  }
+
+  public async disconnect(): Promise<void> {
+    if (this.client) {
+      await this.client.disconnect();
+      this.client = null;
+    }
   }
 }
 
-const redisClient = new RedisService();
+const redisClient = RedisService.getInstance();
 export default redisClient;
